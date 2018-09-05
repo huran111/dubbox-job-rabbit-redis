cf7caf2060971f37ad700a632b3ef79de6094d95
BUCHU <710625937@qq.com>
2018-09-05 18:16:56 +0800

    提交人：胡冉 提交内容：添加消费

--- xz_lwjk_event_facade/src/main/java/com/xz/lwjk/event/facade/job/UserSyncJob.java
+++ xz_lwjk_event_facade/src/main/java/com/xz/lwjk/event/facade/job/UserSyncJob.java
@@ -1,16 +1,9 @@
 package com.xz.lwjk.event.facade.job;
-
 import com.dangdang.ddframe.job.api.JobExecutionMultipleShardingContext;
 import com.dangdang.ddframe.job.plugin.job.type.simple.AbstractSimpleElasticJob;
-import com.event.common.entity.User;
-import com.xz.lwjk.event.facade.common.service.IBaseService;
-import com.xz.lwjk.event.facade.service.IUserService;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 
 /**
  * @author huran
@@ -20,12 +13,11 @@ import java.util.List;
 @Component
 @Slf4j
 public class UserSyncJob extends AbstractSimpleElasticJob{
-    @Autowired
-    IUserService userService;
+
 
     @Override
     public void process(JobExecutionMultipleShardingContext jobExecutionMultipleShardingContext) {
-      List<User> list=  userService.findAll();
-       log.info("用户信息为：[{}]",list.toString());
+
+       log.info("用户信息为：[{}]");
     }
 }
